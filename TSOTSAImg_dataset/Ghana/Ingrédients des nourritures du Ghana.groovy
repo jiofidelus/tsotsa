d77e36668b19289dda59b2017d22da8485e358d9
Azanzi Jiomekong <jiofidelus@gmail.com>
2024-04-17 11:03:39 +0200

    The first version of image dataset collected by students

---
+++ TSOTSAImg_dataset/Ghana/Ingrédients des nourritures du Ghana.groovy
@@ -0,0 +1,228 @@
+Ingrédients des nourritures du Ghana 
+
+1. nkatsenkwan: 
+700g de viande d'agneau en cubes (collier avec os pour moi)
+800g de carottes, pelées et coupées en biseaux assez épais
+1 oignon rouge, émincé finement
+1 cuill à soupe de gingembre râpé
+1 gousse d'ail, pressée
+1 cuill à soupe de piment de cayenne
+1 cuill à café de curry de madras
+2 cuill à café de sel
+1 cuill à café de poivre noir moulu
+1 piment africain (type scotch bonnet)
+2-3 cuill à soupe de beurre de cacahuètes
+Cacahuètes grillées salées grossièrement hachées pour garnir
+
+Pour la sauce Chalé:
+1 boîte de tomates en cubes (400g)
+1 oignon rouge, émincé finement
+1 cuill à soupe de gingembre râpé
+3 gousses d'ail, pressée
+1/2 piment africain (scotch bonnet ou habanero)
+1 cuill à café de curry de madras
+1/2 bouillon cube (ou un peu de bouillon de poulet concentre
+
+2. Pain Sucrée Ghanéen 
+
+300 gr de farine24
+50 gr de cassonade
+11 cl d’eau tiède
+50 gr de beurre mou ou de margarine
+7 gr de levure sèche de boulanger
+1 c. à café rase de sel
+1/2 c. à café de poudre de muscade
+
+3. Ragout de viande au GOmbo 
+
+400 g de bœuf pour bourguignon
+400 g de gombos
+1 gros oignon
+1 poivron vert
+1 piment Scotch bonnet
+200 g de tomates concassées
+1 c. à s. de concentré de tomates
+2 c. à s. de poudre d'écrevisse
+2 cubes de bouillon Kub
+4 c. à s. d'huile de palme rouge
+200 ml d'eau
+Sel
+
+4. Sauce d'épinard à la viance 
+
+500 gr de bœuf (type basse côte, rumsteak)
+3 tomates fraîches
+1 c. à soupe de concentré de tomate
+1 oignon
+2 gousses d’ail,
+1 c. à soupe de gingembre frais
+50 cl de bouillon de légumes
+½ c. à café du piment de Cayenne
+Sel
+600 gr d’épinards en branches (surgelé ou pas)
+Huile
+
+5. Brochette d'agneau au cacahuètes
+
+Pour le rub 'Suya':
+150g de cacahuètes grillées
+2 cuill à café de piment en poudre
+0,5 cuill à café de pimenton
+1 cuill à café de gingembre en poudre
+1 cuill à café de muscade africaine (Monodora myristica) ou de muscade classique rapée
+1 cuill à café d'ail en poudre
+0,5 cuill à café de clous de girofle réduits en poudre
+0,5 cuill à café de cannelle en poudre
+0,5 cuill à café de sel
+
+Pour les brochettes:
+400g d'agneau coupé en cubes de 3cm de côté environ (de l'épaule par exemple)
+1 oignon rouge
+2 poivrons (un vert, un rouge)
+3 cuill à soupe d'huile végétale
+1 cuill à café de sel
+1 cuill à café de poivre moulu
+
+6. Pancake plantain 
+
+1 banane plantain très mûre (noire) ou deux bananes classiques bien mûres
+1 demi oignon, réduit en purée (je le passe à la râpe moyenne)
+1 cuill à café de gingembre râpé
+1/8 de piment africain finement haché ('Scotch bonnet' - piment antillais/habanero à défaut)
+1 pincée de piment de cayenne
+2 belles pincées de sel
+3 cuill à soupe de farine de riz
+1 cuill à soupe d'huile de palme (ou huile végétale de votre choix)
+
+7. Haricot en sauce ROuge
+
+250g de haricots aux yeux noirs secs ('black eyed beans')
+5 cuill à soupe d'huile de palme (équitable si vous pouvez, voir les conseils de Pekopeko en commentaire)
+1 ou 2 oignons rouges
+1 bonne cuill à soupe de gingembre frais râpé
+un demi piment africain (Scotch Bonnet)
+2 cuill à café de piment en paillettes
+0,5 cuill à café de piment en poudre
+0,5 cuill à café de mélange d'épices à curry
+1 boîte de tomates pelées au jus
+1 boîtes de tomates cerises au jus (ou fraîches en saison)
+1 cuill à soupe de concentré de tomates
+1 cuill à café de sel
+0,5 cuill à café de poivre noir fraîchement moulu
+gari (farine de manioc fermenté pour garnir)
+
+
+8. Fetri Detsi
+
+Voici les ingrédients de la recette :
+400 g de bœuf pour bourguignon
+400 g de gombos
+1 gros oignon
+1 poivron vert
+1 piment Scotch bonnet
+200 g de tomates concassées
+1 c. à s. de concentré de tomates
+2 c. à s. de poudre d'écrevisse
+2 cubes de bouillon Kub
+4 c. à s. d'huile de palme rouge
+200 ml d'eau
+Sel
+
+9. Shoko 
+
+600 gr d’épinards en branches (surgelé ou pas)
+500 gr de bœuf (type basse côte, rumsteak)
+3 tomates fraîches
+1 c. à soupe de concentré de tomate
+1 oignon
+2 gousses d’ail,
+1 c. à soupe de gingembre frais
+50 cl de bouillon de légumes
+½ c. à café du piment de Cayenne
+Sel
+Huile
+
+9. Jollof Rice
+
+2 gros oignons jaunes,
+3 gousses d’ail,
+⅓ tasse d’huile végétale (80 ml),
+2 boîtes de tomates en dés (395 g),
+1 boîte de concentré de tomates (140 g),
+1 piment habanero,
+2 cuillères-à-café de curry en poudre,
+1 cuillère-à-café d’ail en poudre,
+1 cuillère-à-café de gingembre moulu,
+1 poignée de coriandre,
+2 cubes de bouillon,
+500g de riz à grain long,
+1 carotte moyenne,
+100 g de petits pois surgelés,
++'/'- 360 ml d’eau,
+Sel et Poivre.
+
+10. Waake 
+
+1 oignon
+1 c à s d'huile d'olive
+200g de riz blanc
+400g de haricots rouge poids net égoutté 250g
+2 feuilles de laurier
+1 c à c de poivre noir
+sel
+1 c à s de concentré de tomates
+300g d'eau
+
+11. Galette de banane plantains
+
+1 banane plantain bien mûre
+25 g de semoule de maïs ou de fariné de lin
+1 oignon vert émincé
+1 c. à c. de gingembre frais ou en poudre
+3 c. à s. de jus de citron
+1 pincée de piment
+1 pincée de poivre de sélim (à défaut, du poivre de Cayenne)
+Sel
+
+12. Poulet Palava
+
+2 escalopes de poulet
+500 g d' épinards
+400 g de dés de tomate
+2 oignons
+1 peu d' de huile d'olive
+1 cc de thym
+2 cs de purée de cacahuètes
+30 cl de bouillon de volaille
+sel et poivre
+1 pincée de piment
+
+13. Suya
+
+22 g de gingembre en poudre
+30 g de cacahuètes grillées
+2 petits bouillons Kub
+2 piments de Cayenne (ou pas)
+5 g de sel fin
+10 g d'ail en poudre
+7 g d'oignon en poudre
+3 g de poivre
+1 cuillère à soupe de paprika
+Huile d’arachide pour la friture
+
+14. Salade composées aux sardines
+
+2 carottes
+1 petite salade verte
+1 pomme de terre
+1 petite boîte de pois (chiches)
+1 boîte de sardines à l'huile
+1/3 de poivron rouge
+1/3 de poivron jaune
+1/3 de poivron vert
+1/2 concombre
+1 oignon frais
+1 branche de céleri
+2 œufs
+6 cuillères à soupe de mayonnaise
+Sel au goût
